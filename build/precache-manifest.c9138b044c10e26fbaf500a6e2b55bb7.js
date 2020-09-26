51e30d566b33f5485a15bd6e3addc06070aa0977
Meghanath Nalawade <50794140+mr-compilerMS@users.noreply.github.com>
2020-09-26 13:11:44 +0530

    Build Publish

---
+++ build/precache-manifest.c9138b044c10e26fbaf500a6e2b55bb7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a01ffa2af17197adeb3e9c3704b5baef",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fb83c0aa62b82589b513",
+    "url": "/static/css/2.85b71462.chunk.css"
+  },
+  {
+    "revision": "0687a0f9339f1c6a2861",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "fb83c0aa62b82589b513",
+    "url": "/static/js/2.619579f3.chunk.js"
+  },
+  {
+    "revision": "6c661bfd12b151c09a654fe75d1650f7",
+    "url": "/static/js/2.619579f3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0687a0f9339f1c6a2861",
+    "url": "/static/js/main.229f3101.chunk.js"
+  },
+  {
+    "revision": "950e66d9a72cf7004ed1",
+    "url": "/static/js/runtime-main.42b43c7a.js"
+  }
+]);
